652ecf33229f1c40b6b02bace24338df7a84ce78
SARA3SAEED <Swaretoo@gmail.com>
2024-06-29 19:09:53 +0300

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,6 +14,8 @@ export default function HomePage() {
       <Nav />
 
       <div className="flex flex-wrap">
+
+
         <div className="hero bg-base-100 min-h-screen">
           <div className="hero-content flex-col lg:flex-row-reverse">
             <div className="text-center lg:text-left">
@@ -23,55 +25,61 @@ export default function HomePage() {
               </p>
               <a href="/signup" className=" mx-10 my-4 btn bg-teal-600 text-white py-2 px-4 rounded transition-colors duration-300 hover:bg-teal-700 bg-opacity-70">Get Started</a>
             </div>
-            <div className="card bg-base-100 w-[50%] shrink-0 shadow-2xl">
-              <div className="card-body">
+              <div className="card-body shadow-1xl">
                 <img src={img1} alt="Image 1" />
-              </div>
             </div>
           </div>
         </div>
+
+
         <hr className="my-8 mx-60 border-gray-500 w-[60%] " />
         
 
         <div className="hero bg-base-100 min-h-screen">
-          <div className="hero-content flex-col lg:flex-row">
-            <div className="text-center lg:text-left">
-              <small className="m-5 pl-10 pr-20 text-teal-600 font-bold">ORGANIZED</small>
-              <h1 className="text-4xl px-8 m-5 font-[600]">Intuitive Task Management</h1>
-              <p className="m-5 pl-10 pr-20 font-[450]">
+          <div className="hero-content  flex-col lg:flex-row">
+            <div className="text-center m-5 p-5 lg:text-left">
+              <small className=" text-teal-600 font-bold">ORGANIZED</small>
+              <h1 className="text-4xl  font-[600]">Intuitive Task Management</h1>
+              <p className=" font-[450]">
                 Easily create, assign, and track tasks with our user-friendly interface. Stay organized and keep your team on the same page.
               </p>
             </div>
-            <img src={img2} className="mr-12 shadow-2xl" alt="Image 2" />
+
+            <img src={img2} className="m-5 w-[45%] shrink-0 shadow-xl" alt="Image 2" />
+
           </div>
         </div>
 
 
         <div className="hero bg-base-100 min-h-screen">
           <div className="hero-content flex-col lg:flex-row-reverse">
-            <div className="text-center lg:text-left">
-              <small className="m-5 pl-10 pr-20 text-teal-600 font-bold">COLLABORATION</small>
-              <h1 className="text-4xl px-8 m-5 font-[600]">Collaborative Workspaces</h1>
-              <p className="m-5 pl-10 pr-20 font-[450]">
+            <div className="text-center m-5 p-5 lg:text-left">
+              <small className=" text-teal-600 font-bold">COLLABORATION</small>
+              <h1 className="text-4xl  font-[600]">Collaborative Workspaces</h1>
+              <p className=" font-[450]">
                 Foster collaboration by creating dedicated workspaces for your projects. Share files, updates, and feedback in one central location.
               </p>
             </div>
-            <img src={img3} className="ml-12 shadow-2xl" alt="Image 3" />
+
+            <img src={img3} className="m- w-[45%] shrink-0 shadow-xl" alt="Image 3" />
           </div>
+
         </div>
 
 
         <div className="hero bg-base-100 min-h-screen">
           <div className="hero-content flex-col lg:flex-row">
-            <div className="text-center lg:text-left">
-              <small className="m-5 pl-10 pr-20 text-teal-600 font-bold">VISIBILITY</small>
-              <h1 className="text-4xl px-8 m-5 font-[600]">Real-Time Progress Tracking</h1>
-              <p className="m-5 pl-10 pr-20 font-[450]">
+            <div className="text-center m-5 p-5  lg:text-left">
+              <small className=" text-teal-600 font-bold">VISIBILITY</small>
+              <h1 className="text-4xl  font-[600]">Real-Time Progress Tracking</h1>
+              <p className=" font-[450]">
                 Monitor project progress in real-time with interactive dashboards. Identify bottlenecks and ensure projects stay on schedule.
               </p>
             </div>
-            <img src={img4} className="mr-14 shadow-2xl" alt="Image 4" />
+
+            <img src={img4} className="m-5 w-[35%] shrink-0 shadow-xl" alt="Image 4" />
           </div>
+
         </div>
 
 
@@ -84,12 +92,14 @@ export default function HomePage() {
             </a>
             <p className="max-w-md mx-auto mt-4  dark:text-gray-400  font-[600] text-4xl "> Manage any project, with TaskFlow</p>
             <div className="flex flex-col mt-4 sm:flex-row sm:items-center sm:justify-center">
-            <button className="flex items-center justify-center order-1 w-full  py-2 mt-3 text-sm btn tracking-wide text-gray-600 capitalize transition-colors duration-300 transform border rounded-md sm:mx-2 dark:border-gray-400 dark:text-gray-300 sm:mt-0 sm:w-auto hover:bg-gray-50 focus:outline-none focus:ring dark:hover:bg-gray-800 focus:ring-gray-300 focus:ring-opacity-40">
-                <Link className="text-gray-700 dark:text-gray-200 lg:px-2 dark:hover:text-teal-600 w-[80px] hover:text-teal-600" to="/login">Login</Link>
-            </button>
-                <button className="w-full px-5 py-2 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform btn bg-teal-600 rounded-md sm:mx-2 sm:order-2 sm:w-auto  focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80 mx-10 my-4 btn bg-teal-600 text-white py-2 px-4 rounded transition-colors duration-300 hover:bg-teal-700 bg-opacity-70">
-                <Link className="text-gray-700 text-white dark:text-gray-200 lg:px-2 " to="/signup">Sign up</Link></button>
+                <button className="btn flex items-center justify-center order-1 w-full py-2 mt-3 text-sm tracking-wide capitalize transition-colors duration-300 transform border rounded-md sm:mx-2 sm:mt-0 sm:w-auto focus:outline-none focus:ring focus:ring-opacity-40 mx-3 my-4; bg-gray-50 hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <Link className="text-gray-700 dark:text-gray-200 lg:px-2 dark:hover:text-teal-600 w-[80px] hover:text-teal-600" to="/login">Login</Link>
+                </button>
+                <button className="btn flex items-center justify-center order-1 w-full py-2 mt-3 text-sm tracking-wide capitalize transition-colors duration-300 transform border rounded-md sm:mx-2 sm:mt-0 sm:w-auto focus:outline-none focus:ring focus:ring-opacity-40 mx-3 my-4; bg-teal-600 text-white hover:bg-teal-700 focus:ring-blue-300">
+                    <Link className="text-gray-700 text-white dark:text-gray-200 lg:px-2" to="/signup">Sign up</Link>
+                </button>
             </div>
+
             <p className=" p-5 mt-5 text-sm text-gray-500">© Copyright 2021. All Rights Reserved.</p>
         </div>
         <hr className="my-10 border-gray-200 dark:border-gray-700" />
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,28 +1,23 @@
 import React from 'react';
 import Nav from '../components/Nav';
 
-
 export default function LoginPage() {
   return (
     <>
-    <Nav />
+      <Nav />
       <div className="flex items-center justify-center w-full max-w-7xl mx-auto p-4">
-        <div className="p-8 my-16 bg-white bg-opacity-90 rounded-3xl shadow-2xl relative overflow-hidden w-[768px] max-w-full min-h-[580px]">
-          <div className="absolute top-0 left-0 h-full w-1/2 z-10">
-            <form className="bg-white bg-opacity-90 flex flex-col items-center justify-center p-10 h-full">
+        <div className="my-16 bg-white bg-opacity-90 rounded-3xl shadow-2xl relative overflow-hidden w-full lg:w-[768px] max-w-full min-h-[580px]">
+          <div className="flex flex-col lg:flex-row">
+          <div className="flex lg:h-[600px] h-[250px] flex-col lg:rounded-tl-3xl lg:rounded-bl-3xl  items-center justify-center p-2 w-full lg:w-1/2 bg-gradient-to-r from-teal-500 to-teal-700 text-white">
+              <h1 className="text-3xl p-5 font-bold">Welcome Back!</h1>
+              <p className="text-lg leading-5 p-5 tracking-wide my-5 text-center">Enter your personal details to continue your work</p>
+            </div>
+
+            <div className="bg-white bg-opacity-90 flex flex-col items-center justify-center p-10 w-full lg:w-1/2">
               <h1 className="font-primary text-2xl font-medium">Login</h1>
               <input type="text" placeholder="Username" className="font-primary bg-gray-200 bg-opacity-70 border-none mt-4 p-3 text-sm rounded-lg w-full outline-none" />
               <input type="password" placeholder="Password" className="font-primary bg-gray-200 bg-opacity-70 border-none mt-4 p-3 text-sm rounded-lg w-full outline-none" />
               <button type="submit" className="btn bg-teal-600 bg-opacity-70 text-white mt-2 py-2 px-4 rounded transition-colors duration-300 hover:bg-teal-800 hover:bg-opacity-100">Login</button>
-            </form>
-          </div>
-
-          <div className="toggle-container absolute top-0 left-1/2 w-1/2 h-full overflow-hidden rounded-[150px_0_0_100px] z-[1000]">
-            <div className="toggle bg-gradient-to-r from-teal-500 to-teal-700 bg-opacity-120 text-white relative left-[-100%] h-full w-[200%]">
-              <div className="toggle-panel bg-opacity-20 absolute right-0 w-1/2 h-full flex flex-col items-center justify-center p-8 text-center">
-                <h1 className="text-3xl font-bold">Welcome Back!</h1>
-                <p className="text-lg leading-5 tracking-wide my-5">Enter your personal details to continue your work</p>
-              </div>
             </div>
           </div>
         </div>
